f601450d4275d8a341437912364d8ecbb53df149
najlaasz <najlaa.alzabi@gmail.com>
2025-05-20 19:40:45 +0300

    task 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
 
     defaultConfig {
         applicationId = "com.example.myapplication"
-        minSdk = 34
-        targetSdk = 35
+        minSdk = 26
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,6 +18,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,6 +31,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlin.collections.elementAt
 
 class MainActivity : ComponentActivity() {
 
@@ -36,26 +42,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold() { innerPadding ->
-                    Column (
+                QuizApp()
 
-                        verticalArrangement = Arrangement.SpaceBetween,
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-
-                    ){
-                        Quastions(
-                            name = myQuestions.keys.first()
-                        )
-
-//                        AnswerButton()
-
-
-
-                    }
-                }
             }
         }
     }
@@ -70,6 +58,69 @@ val myQuestions = mapOf(
 
 
 @Composable
+fun QuizApp(){
+    Scaffold() { innerPadding ->
+
+        var questionIndex by remember { mutableStateOf(0) }
+        val questionList = myQuestions.keys.toList()
+        var userValue by remember { mutableStateOf<Boolean?>(null)} // when null Not yet answerd .. at
+        val correctAnswer = myQuestions.values.elementAt(questionIndex)
+
+
+
+        Column (
+
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+
+        ){
+            Quastions(name = questionList[questionIndex])
+
+
+            // getting correct Circle Shape
+            // if answer not null show circle && after each question make userValue = null
+            userValue?.let {
+                if(userValue == myQuestions.values.elementAt(questionIndex))
+                {AnswerCircle(true)}
+                else
+                {AnswerCircle(false)}
+            }
+
+
+
+
+            AnswerButton(
+                trueSelected = { userValue = true },
+                falseSelected = { userValue = false }
+            )
+//            AnswerButton(trueSelected = true,
+//                falseSelected = false)
+
+
+
+            if (userValue != null && userValue == correctAnswer && questionIndex < questionList.size - 1)
+            {
+                Button(onClick = {
+                    questionIndex++
+                    userValue = null
+                }) {
+                    Text(text = "Next Question")
+                }
+
+            }
+
+
+
+        }
+    }
+}
+
+
+
+@Composable
 fun Quastions(name: String, modifier: Modifier = Modifier) {
     Text(
 
@@ -81,27 +132,23 @@ fun Quastions(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun AnswerButton(quastion: String){
+fun AnswerButton(trueSelected:()-> Unit ,
+                 falseSelected:()-> Unit){
     Row (
-        horizontalArrangement = Arrangement.SpaceBetween,
+        horizontalArrangement = Arrangement.SpaceEvenly,
         verticalAlignment = Alignment.CenterVertically) {
-//        Button(onClick = {AnswerCircle(myQuestions[quastion] == true) }) {
-//            Text(text = "True")
-//        }
-//
-//
-//        Button(onClick = { AnswerCircle(myQuestions[quastion] == false) }) {
-//            Text(text = "False")
-//        }
+
+        Button(onClick = {trueSelected()}) {    // means button value = true
+            Text(text = "True")
+        }
+
+        Button(onClick = {falseSelected() }) {
+            Text(text = "False")
+        }
     }
 }
 
-@Composable
-fun NextQuastionButton(){
-    Button(onClick = {}) {
-        Text(text = "Next Quastion")
-    }
-}
+
 
 @Composable
 fun AnswerCircle(answer: Boolean){
@@ -118,16 +165,30 @@ fun AnswerCircle(answer: Boolean){
 
     ){
         Text(
-            modifier = Modifier.fillMaxSize()
-                .background(color = Color.Blue),
+            modifier = Modifier
+                .align(Alignment.Center)
+                .background(color = c),
             textAlign = TextAlign.Center,
             fontSize = 20.sp,
             color = Color.White,
             text = t
-            )
+        )
     }
 
 }
+
+//@Composable
+//fun NextQuastion(userAnswer: Boolean, correctAnswer: Boolean,questionIndex: Int )
+//{ if (userAnswer != null && userAnswer == correctAnswer && questionIndex < questionList.size - 1)
+//{
+//    Button(onClick = {
+//        questionIndex++
+//        userAnswer = null
+//    }) {
+//        Text(text = "Next Question")
+//    }
+//
+//}}
 
 @Preview(showBackground = true)
 @Composable
